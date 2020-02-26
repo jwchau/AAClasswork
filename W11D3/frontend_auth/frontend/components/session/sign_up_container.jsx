6f0f7391048eb761dac2db353fec3844bce908d1
John C <john.win.chau@gmail.com>
2020-02-26 11:21:10 -0800

    front-end auth

---
+++ W11D3/frontend_auth/frontend/components/session/sign_up_container.jsx
@@ -0,0 +1,11 @@
+import { connect } from "react-redux";
+import { createNewUser } from "../../actions/session";
+import Signup from "./sign_up";
+
+const mapDispatchToProps = dispatch => ({
+    createNewUser: formUser => dispatch(createNewUser(formUser)),
+});
+
+
+
+export default connect(null, mapDispatchToProps)(Signup);
