3916108a38fa8841ea0bf054aabc46b87d191095
John C <john.win.chau@gmail.com>
2020-02-27 19:39:01 -0800

    part 1 done

---
+++ W11D4/bnb/app/controllers/api/users_controller.rb
@@ -0,0 +1,25 @@
+class Api::UsersController < ApplicationController
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            render 'api/users/show'
+        else
+            render json: ["failed to create user"], status: 422
+        end
+    end
+
+end
+
+
+# $.ajax({
+#     method: "POST",
+#     url: "/api/users",
+#     data: {
+#         user: {
+#             username: "joawn",
+#             password: "jswoianson"
+#         }
+#     }
+# })
